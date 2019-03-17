6f82acfa7e7d3f44f238e99dbe66286aef596fca
Chris Rimmer <chris@rimmer.wtf>
2019-03-17 00:55:06 +0000

    testing

---
+++ bootstraps/1.yay.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+mkdir ~/aur/
+git clone https://aur.archlinux.org/yay.git ~/aur/yay
+
+cd ~/aur/yay
+makepkg -si
+
--- home/bin/rebuild-dotlinks.sh
+++ home/bin/rebuild-dotlinks.sh
@@ -1,3 +1,1 @@
-pushd ~
 for file in `ls -A --color=never ~/.dots/home`; do ln -sf ~/.dots/home/$file; done;
-popd
